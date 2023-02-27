8db8663aa260c1baccddfe7e7a682677764830fc
Sadaffatima94 <79504509+Sadaffatima94@users.noreply.github.com>
2023-02-27 05:58:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function a(){
+    document.getElementById('para').style.color="red";
+    document.getElementById('b').style.backgroundColor="yellow";
+    document.getElementById('about').style.color="green";
+    document.getElementById('para').style.fontSize="35px";
+}
